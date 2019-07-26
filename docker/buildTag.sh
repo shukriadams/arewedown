327865a03b3bb734ecc2961960ffb7db1de39011
Shukri Adams <shukri.adams@gmail.com>
2019-07-26 13:05:52 +0200

    cleaned build script

--- docker/buildTag.sh
+++ docker/buildTag.sh
@@ -1,3 +1,11 @@
+# This script builds this project at a pre-existing tag, creates a docker image from the build, and the pushes
+# that image to the official container repo. 
+#
+# Use :
+# cd /docker
+# sh ./buildTag x.y.z (where x.y.z) is the tag to build.
+
+
 # tag must be passed in as an argument when calling this script
 TAG=$1
 
@@ -10,34 +18,40 @@ fi
 rm -rf .clone &&
 git clone --depth 1 --branch $TAG https://github.com/shukriadams/amidown.git .clone &&
 
-# copy source code and keys into build container and compile it.
-mkdir -p ./.keys && 
-cp ~/.ssh/* -R ./.keys &&
 
 # kill any existing build container
 docker-compose -f docker-compose-build.yml kill &&
-
 docker-compose -f docker-compose-build.yml up -d &&
+
 
 # clean out build container incase it's been previous used
 docker exec buildcontainer sh -c 'rm -rf /tmp/build/*' &&
 docker exec buildcontainer sh -c 'rm -rf /tmp/stage/*' &&
 
+
 # copy tag clone into buildcontainer
 docker cp ./.clone/src/. buildcontainer:/tmp/build &&
+
+
 # install with --no-bin-links to avoid simlinks, this is needed to copy build content around
 docker exec buildcontainer sh -c 'cd /tmp/build/ && npm install --no-bin-links' &&
+
+
 # copy build to stage folder, clean out node_modules and _fe because these are needed for build stage only
 docker exec buildcontainer sh -c 'cp -R /tmp/build/* /tmp/stage && rm -rf /tmp/stage/node_modules' &&
+
+
 # do a fresh npm install of production-only modules
 docker exec buildcontainer sh -c 'cd /tmp/stage/ && npm install --production --no-bin-links' &&
+
+
 # zip the build up
 docker exec buildcontainer sh -c 'tar -czvf /tmp/build.tar.gz /tmp/stage' &&
 docker cp buildcontainer:/tmp/build.tar.gz . &&
 
 docker build -t shukriadams/amidown . &&
 docker tag shukriadams/amidown:latest shukriadams/amidown:$TAG &&
-#docker push shukriadams/amidown:latest &&
-#docker push shukriadams/amidown:$TAG &&
+docker push shukriadams/amidown:latest &&
+docker push shukriadams/amidown:$TAG &&
 
 echo "Build done";
