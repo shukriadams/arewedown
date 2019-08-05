e24b91ed4a7aeb3204b1c024d05a70858ad8f64e
Shukri Adams <shukri.adams@gmail.com>
2019-08-05 10:35:40 +0200

    removed flicker
    added a soft fix for screen update flicker when rendering on devices with slow screen draw time

--- src/public/js/iframe.js
+++ src/public/js/iframe.js
@@ -1,13 +1,26 @@
 (function(){
     var clientRefreshInterval = document.querySelector('body').getAttribute('data-clientRefreshInterval'),
-        frame = document.querySelector('.contentFrame');
+        activeFrame = document.querySelector('.contentFrame1'),
+        inactiveFrame = document.querySelector('.contentFrame2');
 
     if (clientRefreshInterval)
         clientRefreshInterval = parseInt(clientRefreshInterval);
     
-    if (clientRefreshInterval){
-        setInterval(function(){
-            frame.contentWindow.location = '/status'
-        }, clientRefreshInterval);
+    function update(){
+        inactiveFrame.contentWindow.location = '/status'
+        inactiveFrame.onload = function(){
+            setTimeout(function(){
+                inactiveFrame.classList.add('iframe--show');
+                activeFrame.classList.remove('iframe--show');
+                inactiveFrame = activeFrame;
+                activeFrame = document.querySelector('.iframe--show');
+            }, 500);
+        }
     }
+
+    if (clientRefreshInterval){
+        setInterval(update, clientRefreshInterval);
+    }
+
+    update();
 })()
