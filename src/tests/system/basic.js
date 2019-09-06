0e3094d3cebbe870493e682e477f31b94ef54653
Shukri Adams <shukri.adams@gmail.com>
2019-09-06 10:07:46 +0200

    replaced proper HTTP check accidentally removed from last release

--- src/tests/system/basic.js
+++ src/tests/system/basic.js
@@ -1,5 +1,25 @@
-const httpUtils = require('madscience-httputils');
+const request = require('request');
+
+async function checkHTTPResponse (url){
+    return new Promise((resolve, reject)=>{
+        let code = null;
+
+        request( { uri: url }, 
+            function(error, response) {
+                if (error)
+                    return reject(error);
+                
+                if (code && (code < 200 || code >= 300)) // allow all code 2**
+                    return reject(`Error : server responded with code ${code}.`);
+
+                resolve(response);
+            }
+        ).on('response', function(response) {
+            code = response.statusCode;
+        })
+    });
+};
 
 module.exports = async function(job){
-    await httpUtils.downloadString(job.config.url);
+    await checkHTTPResponse(job.config.url);
 }
