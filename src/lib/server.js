28354c098c98f6b98b123733d09a61ee36b879b8
Shukri Adams <shukri.adams@gmail.com>
2021-05-19 01:40:43 +0200

    split server start logic up into more functions

--- src/lib/server.js
+++ src/lib/server.js
@@ -14,8 +14,6 @@ module.exports = {
             Express = require('express'),
             express = Express(),
             settings = require('./settings'),
-            sh = require('madscience-node-exec').sh,
-            log = require('./logger').instance(),
             startArgs = require('./startArgs').get()
         
 
@@ -24,32 +22,82 @@ module.exports = {
         // responsible for writing that tag to package.json. 
         // When running in dev mode, version always returns the placeholder value of "0.0.1", which must never
         // be updated.
-        if (startArgs.version){
-            const package = await fs.readJson(`${__dirname}/../package.json`)
-            console.log(`AreWeDown? v${package.version}`)
-            return package.version // return code is test aid
-        }            
+        if (startArgs.version)
+            return this.printVersion() // return code is test aid
 
+        await this.executeStartScript()
         
-        // Execute onstart shell command - this is intended for docker builds where the user wants to 
-        // install app or set state in the container, but doesn't want to bake their own container image.
-        // This state is ephemeral, so this solution isn't optimal, but it at least docker novices a chance
-        // to run shell commands with advanced requirements. Options are nice.
-        if (settings.onstart){
-            log.info('onstart command executing')
-    
-            try {
-                const result = await sh({ cmd : settings.onstart })
-                log.info(`onstart finished with result : `, result)
-            } catch(ex){
-                throw { text : `onstart failed with error : `, ex }
-            }
-        }
-    
+        // ensure/validate all the things
         await fs.ensureDir(settings.logs)
+        await this.validateTransports()
 
 
-        // validate active transport's settings by attempting to contact provider 
+        // ready to start - load/start all the things
+        await this.loadRoutes(express)
+        await daemon.start()
+        
+        server = http.createServer(express)
+        serverProcess = server.listen(settings.port)
+
+        console.log(`Are We Down? started, listening on port ${settings.port}`)
+    },
+
+
+    /**
+     * 
+     */
+    async printVersion(){
+        const package = await fs.readJson(`${__dirname}/../package.json`)
+        console.log(`AreWeDown? v${package.version}`)
+        return package.version 
+    },
+    
+
+    /**
+     * Execute onstart shell command - this is intended for docker builds where the user wants to 
+     * install app or set state in the container, but doesn't want to bake their own container image.
+     * This state is ephemeral, so this solution isn't optimal, but it at least docker novices a chance
+     * to run shell commands with advanced requirements. Options are nice.
+     */
+    async executeStartScript(){
+        const settings = require('./settings'),
+            sh = require('madscience-node-exec').sh,
+            log = require('./logger').instance()
+
+        if (!settings.onstart)
+            return
+
+        log.info('onstart command executing')
+
+        try {
+            const result = await sh({ cmd : settings.onstart })
+            log.info(`onstart finished with result : `, result)
+        } catch(ex){
+            throw { text : `onstart failed with error : `, ex }
+        }
+    },
+
+
+    /**
+     * load express routes - these are all files in .src/routes folder
+     */
+    async loadRoutes(express){
+        const routeFiles = await this.findRoutes()
+        for (const routeFile of routeFiles){
+            const routeFileName = routeFile.match(/(.*).js/).pop(),
+                route = require(`./../routes/${routeFileName}`)
+    
+            route(express)
+        }
+    },
+
+
+    /**
+     * validate active transport's settings by attempting to contact provider 
+     */
+    async validateTransports(){
+        const settings = require('./settings')
+
         for (const transportName in settings.transports){
             if (!settings.transports[transportName].enabled)
                 continue
@@ -60,26 +108,9 @@ module.exports = {
 
             await transport.ensureSettingsOrExit()
         }
-        
-
-        // load express routes - these are all files in .src/routes folder
-        const routeFiles = await this.findRoutes()
-        for (const routeFile of routeFiles){
-            const routeFileName = routeFile.match(/(.*).js/).pop(),
-                route = require(`./../routes/${routeFileName}`)
-    
-            route(express)
-        }
-        
-        await daemon.start()
-        
-        server = http.createServer(express)
-        serverProcess = server.listen(settings.port)
-
-        console.log(`Are We Down? started, listening on port ${settings.port}`)
     },
-
     
+
     /**
      * Break out route loading for easier testing.
      */
